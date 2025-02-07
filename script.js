30c4c5a0d55a8a2c8a5ad353b075d8dbae9078a9
Ashok pal <ashokpal2094@gmail.com>
2025-02-07 10:39:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 let display = document.getElementById('display');
 
-function appendCharacter(char) {
-    display.value += char;
-}
-
 function clearDisplay() {
     display.value = '';
 }
@@ -12,10 +8,14 @@ function deleteLast() {
     display.value = display.value.slice(0, -1);
 }
 
+function appendCharacter(char) {
+    display.value += char;
+}
+
 function calculateResult() {
     try {
         display.value = eval(display.value);
-    } catch (error) {
+    } catch (e) {
         display.value = 'Error';
     }
 }
